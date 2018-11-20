1268178bc00bb7596b691703f1f40a4882655c6a
EricH <AvarioDeveloper@gmail.com>
2018-11-20 10:19:23 -0700

    Early dnd storybook

--- packages/react-bootstrap-table2-example/examples/drag-and-drop/drag-and-drop.js
+++ packages/react-bootstrap-table2-example/examples/drag-and-drop/drag-and-drop.js
@@ -1,10 +1,74 @@
 import React from 'react';
+/* eslint-disable */
+import { DragSource, DropTarget, DragDropContextProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend'
 
 import BootstrapTable from 'react-bootstrap-table-next';
 import Code from 'components/common/code-block';
 import { productsGenerator } from 'utils/common';
 
 const products = productsGenerator();
+
+const source = {
+    beginDrag(props) {
+        console.log('started drag');
+      return {
+        text: props.text
+      };
+    },
+    endDrag(props, monitor, component) {
+        if (!monitor.didDrop()) {
+          return;
+        }
+    
+        // When dropped on a compatible target, do something
+        const item = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+        console.log('dropped from source',item, dropResult);
+      }
+  };
+
+function collectSource(connect, monitor) {
+    return {
+      connectDragSource: connect.dragSource(),
+      isDragging: monitor.isDragging()
+    };
+  }
+
+const Source = ({ isDragging, connectDragSource, connectDropTarget, text }) => {
+    return connectDragSource(
+      <div style={{ opacity: isDragging ? 0.5 : 1 }}>
+        {text}
+      </div>
+    );
+  }
+
+  const target = {
+    drop(props, monitor, component) {
+        console.log('dropped into target', props, monitor, component);
+    }
+  };
+
+  function collectTarget(connect, monitor) {
+    return {
+      // Call this function inside render()
+      // to let React DnD handle the drag events:
+      connectDropTarget: connect.dropTarget(),
+    };
+  }
+
+const Target = ({ connectDropTarget, text }) => {
+    return connectDropTarget(
+        <div>{text}</div>
+    );
+}
+
+const WrappedSource = DragSource('CONST', source, collectSource)(Source);
+const WrappedTarget = DropTarget('CONST', target, collectTarget)(Target);
+
+const dragFormatter = (cell, row, rowIndex) => {
+    return <div><WrappedSource text="X" /><WrappedTarget text="Y" /></div>;
+};
 
 const columns = [{
   dataField: 'id',
@@ -15,7 +79,13 @@ const columns = [{
 }, {
   dataField: 'price',
   text: 'Product Price'
+}, {
+  dataField: 'drag',
+  text: 'Order rows',
+  isDummyField: true,
+  formatter: dragFormatter
 }];
+
 
 const sourceCode = `\
 import BootstrapTable from 'react-bootstrap-table-next';
@@ -36,7 +106,9 @@ const columns = [{
 
 export default () => (
   <div>
-    <BootstrapTable keyField="id" data={ products } columns={ columns } />
+      <DragDropContextProvider backend={HTML5Backend}>
+        <BootstrapTable keyField="id" data={ products } columns={ columns } />
+      </DragDropContextProvider>
     <Code>{ sourceCode }</Code>
   </div>
 );
